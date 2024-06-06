6ed872eee9544406a0247a64508a8eee6abc4c65
jaquelineatienza <jaquelinebatienza@gmail.com>
2024-06-05 22:43:25 -0300

    se corrigio el url de la api

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,31 @@
+const url = 'https://rickandmortyapi.com/api/character';
+
 const procesarPromesa = async (url) => {
     try {
         const respuesta = await fetch(url);
         const info = await respuesta.json();
-        return info; // Retornamos la información para que pueda ser usada en el `then`
+        return info;
+        console.log(info)
     } catch (error) {
         console.log("error");
     }
-}
-const url = 'https://rickandmortyapi.com/api/character';
-const contendor = document.getElementById('contendor'); 
+}  
 
-//funcion para traer los elementos del join 
+
+
+ const contendor = document.getElementById('contendor'); 
+
+// funcion para traer los elementos del join 
 procesarPromesa(url)
     .then((info) => {
         info.results.forEach(elemento => {
             contendor.innerHTML+= `
-                <div class="personaje">
-                <img src="${elemento.image}" />
-                <h2>${elemento.name}</h2>
+                <div class="personajes">
+                    <img src="${elemento.image}" />
+                    <h2>${elemento.name}</h2>
                 </div>`;
         });
+     
     })
     .catch((error) => {
         console.log("Error en la promesa:", error);
